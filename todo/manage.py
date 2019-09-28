ec0648efd47014961e8dbd9f6a7d23f077930430
Anish Mukherjee <anish.mukherjee1996@gmail.com>
2019-09-28 09:21:37 +0530

    Update manage.py

--- todo/manage.py
+++ todo/manage.py
@@ -6,7 +6,6 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todo.settings")
     try:
         from django.core.management import execute_from_command_line
-        runserver.default_port = "8081"
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
